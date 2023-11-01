f3e551f28637dba5e9d98f909fe38b345438a341
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2023-11-01 10:05:15 +0600

    6th commit problem_solving

---
+++ src/problemSolving/Problem1023.java
@@ -0,0 +1,27 @@
+package problemSolving;
+
+import java.io.IOException;
+import java.util.*;
+import static java.lang.Math.sqrt;
+
+public class Problem1023 {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        double pr1 = (b * b) - (4 * a * c);
+
+        if (pr1 < 0 || a == 0) {
+            System.out.println("Impossivel calcular");
+        } else {
+            double bhaskara_formula = (-b + sqrt(pr1)) / (2 * a);
+            double bhaskara_formula2 = (-b - sqrt(pr1)) / (2 * a);
+            System.out.println("R1 = " + String.format("%.5f", bhaskara_formula));
+            System.out.println("R2 = " + String.format("%.5f", bhaskara_formula2));
+        }
+    }
+}
